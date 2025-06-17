79d1d4ad605d02422cb5760e22b197ca83071d8b
emondaca <30303704+emondaca@users.noreply.github.com>
2025-06-17 18:37:32 -0400

    cambia API URL

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,7 @@
-export const URLBASE = 'http://localhost:3000'
+/*desarrollo*/
+/*export const URLBASE = 'http://localhost:3000'*/
+/*producción*/
+export const URLBASE = 'https://proyecto-backend-h0h6.onrender.com'
 
 export const ENDPOINT = {
   login: `${URLBASE}/clientes/login`,
