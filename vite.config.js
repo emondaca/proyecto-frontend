05738e55149844c15cd6625552e0513be4035259
emondaca <30303704+emondaca@users.noreply.github.com>
2025-06-20 01:03:53 -0400

    AllowedHost6

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: "proyecto-frontend-iyts.onrender.com",
+    allowedHosts: ["proyecto-frontend-iyts.onrender.com"],
     port: 5000,
    host: '0.0.0.0',
    }
