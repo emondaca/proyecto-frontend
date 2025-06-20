b8b5ad48d8611d3a217e614371cd3df9d3305c88
emondaca <30303704+emondaca@users.noreply.github.com>
2025-06-20 00:08:10 -0400

    upgrade_20_06

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4200,
+    port: 5000,
    host: '0.0.0.0',
+   allowedHosts: "proyecto-frontend-iyts.onrender.com"
   },
 })
